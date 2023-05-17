5ecafd7170dec534e80969aa2f64f3d506b7beb1
Valendovsky <valendovsky@gmail.com>
2023-05-17 22:13:53 +0400

    Create the project

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Асинхронный Телеграмм-бот на основе aiogram с интегрированным ChatGPT
+
+
+def _print_name(name):
+    print(f'The module name is {name}')
+
+
+if __name__ == '__main__':
+    _print_name(__name__)
+
+# by Valendovsky
